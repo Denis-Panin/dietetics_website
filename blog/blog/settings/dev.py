b5de91c19ceea91a81b02f099dddba924bb8036f
Denis Panin <deniswork2021@ukr.net>
2022-06-30 14:51:25 +0300

    start project

---
+++ blog/blog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-4nmgz^m@_%*^2$d_9)bj6k0b5wvd-&p%5we5o+7wh$6e(2xb3@"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
